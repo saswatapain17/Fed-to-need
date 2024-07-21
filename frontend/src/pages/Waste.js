80f200df9c1f72e4fa8eca7a0b3d98d861db4325
Saswata <65461780+saswatapain17@users.noreply.github.com>
2024-07-22 05:20:41 +0530

    Update Waste.js

--- frontend/src/pages/Waste.js
+++ frontend/src/pages/Waste.js
@@ -31,7 +31,7 @@ const Waste = () => {
       }
 
       const response = await axios.post(
-        "http://localhost:5000/waste",
+        "https://fed-to-need-server.vercel.app/waste",
         FoodWasteData,
         {
           headers: {
